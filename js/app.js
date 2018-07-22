8ba0de7f72bd3f89d1d6a4070ce74620cc10d93e
Sara Krum <sarasmith887@gmail.com>
2018-07-22 19:17:06 -0400

    two cats added with names

--- js/app.js
+++ js/app.js
@@ -1,10 +1,24 @@
 const button = document.getElementById('button');
 const clickCount = document.querySelector('.numClicks');
 let i = 0;
+let catName = document.querySelector('.name');
+const cat1 = 'Luna';
+let catNameTwo = document.querySelector('.nameTwo');
+const cat2 = 'Jynx';
+const buttonTwo = document.getElementById('buttonTwo');
+const clickCountTwo = document.querySelector('.numClicksTwo');
+let j = 0;
 
 button.addEventListener('click', function(){ 
-	//alert('buttons has been clicked');
 	i ++;
 	clickCount.innerHTML = `${i}`;
-	//console.log(clickCount);
 }, false);
+
+//project requires 2 cats now
+buttonTwo.addEventListener('click', function(){ 
+	j ++;
+	clickCountTwo.innerHTML = `${j}`;
+}, false);
+
+catName.innerHTML = `${cat1}`;
+catNameTwo.innerHTML = `${cat2}`;

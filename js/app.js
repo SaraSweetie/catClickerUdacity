58f26bb50fc5baed66244e8713e0d0d7ff713446
Sara Krum <sarasmith887@gmail.com>
2018-07-24 08:52:49 -0400

    psudo code added for project requirements change 3

--- js/app.js
+++ js/app.js
@@ -2,11 +2,24 @@ const button = document.getElementById('button');
 const clickCount = document.querySelector('.numClicks');
 let i = 0;
 let catName = document.querySelector('.name');
-const cat1 = 'Luna';
+
+const catNames = ['Luna', 'Jynx', 'Tabby', 'Smudge', 'Fat Cat'];
 
 button.addEventListener('click', function(){ 
 	i ++;
 	clickCount.innerHTML = `${i}`;
 }, false);
 
-catName.innerHTML = `${cat1}`;
+catName.innerHTML = `${catNames[i]}`;
+
+//To Do:
+
+//define cats in an object with their image url
+
+//push cats names to an ul in the HTML
+//make cat's names links to click and swap the image out
+
+
+//count the number of button clicks
+
+//make sure that the number of clicks restart when a new image is loaded

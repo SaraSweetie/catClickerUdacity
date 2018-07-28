6d88779cdfc931f224959b8d6637174cba110072
Sara Krum <sarasmith887@gmail.com>
2018-07-28 15:14:45 -0400

    re-organizing code to module, octopus, and view

--- js/app.js
+++ js/app.js
@@ -2,6 +2,21 @@ const clickCountButton = document.getElementById('clickCountButton');
 const clicks = document.querySelector('.numClicks');
 let displayName = document.querySelector('.displayName');
 
+/* ======= Model (data) ======= */
+let catsList = [
+	{ catName: 'Luna', src:'https://cdn.pixabay.com/photo/2018/07/13/10/20/cat-3535404_1280.jpg',
+		alt:'alternative image 1 text', attribute:'source of image 1', clickCount:0 },
+	{ catName: 'Jynx', src:'https://cdn.pixabay.com/photo/2017/02/20/18/03/cat-2083492_1280.jpg',
+		alt:'alternative image 2 text', attribute:'source of image 2', clickCount:0 },
+	{ catName: 'Tabby', src:'https://cdn.pixabay.com/photo/2014/04/13/20/49/cat-323262_1280.jpg',
+		alt:'alternative image 3 text', attribute:'source of image 3', clickCount:0 },
+	{ catName: 'Smudge', src:'https://cdn.pixabay.com/photo/2018/04/27/08/59/maine-coon-3354072_960_720.jpg',
+		alt:'alternative image 4 text', attribute:'source of image 4', clickCount:0 },
+	{ catName: 'Fat Cat', src:'https://cdn.pixabay.com/photo/2016/04/25/10/57/cat-1351612_1280.jpg',
+		alt:'alternative image 5 text', attribute:'source of image 5', clickCount:0 }
+];
+
+/* ======= Octopus ??? ======= */
 clickCountButton.addEventListener('click', function(){ 
 	if (imageSrc.src === catsList[0].src){
 		catsList[0].clickCount ++;
@@ -24,16 +39,7 @@ clickCountButton.addEventListener('click', function(){
 	}
 }, false);
 
-//define cats in an object with their image url
-let catsList = [
-	{ catName: 'Luna', src:'https://cdn.pixabay.com/photo/2018/07/13/10/20/cat-3535404_1280.jpg', clickCount:0 },
-	{ catName: 'Jynx', src:'https://cdn.pixabay.com/photo/2017/02/20/18/03/cat-2083492_1280.jpg', clickCount:0 },
-	{ catName: 'Tabby', src:'https://cdn.pixabay.com/photo/2014/04/13/20/49/cat-323262_1280.jpg', clickCount:0 },
-	{ catName: 'Smudge', src:'https://cdn.pixabay.com/photo/2018/04/27/08/59/maine-coon-3354072_960_720.jpg', clickCount:0 },
-	{ catName: 'Fat Cat', src:'https://cdn.pixabay.com/photo/2016/04/25/10/57/cat-1351612_1280.jpg', clickCount:0 }
-];
-
-//push cats names (from catsList array) to an HTML button
+/* ======= View (image buttons) ======= */
 const catButtons = document.getElementById('catButtons');
 let j = 0;
 
@@ -43,13 +49,13 @@ for (let cat in catsList) {
     //console.log(catButtons);
     j++;
 }
-//make cat's names links to click and swap the image out
+
+/* ======= View (cat images, clickCount, and cat name displayed) ======= */
 const cat1 = document.getElementById('cat1');
 const cat2 = document.getElementById('cat2');
 const cat3 = document.getElementById('cat3');
 const cat4 = document.getElementById('cat4');
 const cat5 = document.getElementById('cat5');
-
 const imageSrc = document.getElementById('catImage');
 
     /*let getImage = document.getElementById(cat${j+1});
